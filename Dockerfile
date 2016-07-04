5ff6e22fc20189bab619d110b3b52c52551e0ea8
Yuanhai He <i@bestmike007.com>
2016-07-04 17:21:56 +0800

    Use weave attach to connect container to weave network.

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,11 @@
-FROM docker:1.11
+FROM docker:1.11.2
 MAINTAINER Yuanhai He <i@bestmike007.com>
 
+ENV WEAVE_VERSION=1.6.0 \
+    DOCKER_VERSION=1.11.2
 COPY startup-script.sh /start
-RUN curl -sSL -o /usr/local/bin/weave git.io/weave && \
+RUN curl -sSL -o /usr/local/bin/weave https://github.com/weaveworks/weave/releases/download/v${WEAVE_VERSION}/weave && \
     chmod a+x /usr/local/bin/weave /start && \
-    apk add --no-cache iptables && \
-    cat /usr/local/bin/weave | grep -e ^SCRIPT_VERSION | cut -d'"' -f 2
+    apk add --no-cache iptables
 
 ENTRYPOINT ["/start"]
--- startup-script.sh
+++ startup-script.sh
@@ -1,63 +1,59 @@
 #!/bin/sh
-trap "exit 1" SIGTERM TERM
-trap "echo 'Cleaning up...'; exit 0" SIGHUP SIGINT
 export TOP_PID=$$
+trap "echo 'Force killed...'; exit 1" SIGTERM TERM
+trap "echo 'Cleaning up...'; exit 0" SIGHUP SIGINT INT HUP EXIT SIGQUIT
 NETWORK_NAME=${WEAVE_NETWORK_NAME:-custom}
 
+[ "$1" == "setup" ] && (
+  weave setup
+  kill -s TERM $TOP_PID
+)
+
+echo "Starting weave daemon using docker ${DOCKER_VERSION} and weave ${WEAVE_VERSION}..."
+sleep 3
+
 [ -z "$WEAVE_ROUTER_CMD" ] && (
-  echo "You must specify WEAVE_ROUTER_CMD env. e.g. --no-discovery --no-dns --ipalloc-range 192.168.0.0/24 weave-node-0 weave-node-1"
-  echo "Note: --no-restart is added by default"
+  echo "You must specify WEAVE_ROUTER_CMD env. e.g. --ipalloc-range 192.168.0.0/24 weave-node-0 weave-node-1"
   sleep 60
   kill -s TERM $TOP_PID
 )
 
-[ -z "$WEAVE_SUBNET" ] && (
-  echo "You must specify WEAVE_SUBNET env. e.g. WEAVE_SUBNET=192.168.0.0/24"
-  sleep 60
-  kill -s TERM $TOP_PID
-)
-
-weave setup || exit 1
-weave stop || exit 1
-echo "Starting weave router..."
-weave launch-router $WEAVE_ROUTER_CMD || (
-  echo "Fail to launch router..."
-  sleep 60
-  kill -s TERM $TOP_PID
-)
-
-echo "Starting weave plugin..."
-weave launch-plugin || (
-  echo "Fail to launch plugin..."
-  sleep 60
-  kill -s TERM $TOP_PID
-)
-[ `docker network ls -f name=${NETWORK_NAME} | wc -l` -eq 1 ] && (
-  docker network create -d weavemesh --subnet=${WEAVE_SUBNET} ${NETWORK_NAME} || (
-    echo "Fail to create custom docker network..."
+[ -z "`docker ps -qf ancestor=weaveworks/weave:${WEAVE_VERSION}`" ] && (
+  weave reset --force
+  echo "Starting weave router..."
+  weave launch-router $WEAVE_ROUTER_CMD || (
+    echo "Fail to launch router..."
     sleep 60
     kill -s TERM $TOP_PID
   )
+  weave expose $WEAVE_HOST_CIDR || weave expose
 ) || (
-  echo "Warning: docker network ${NETWORK_NAME} already exists."
+  echo "NOTICE: weave router has already started, if it's not started by weave daemon please manually stop the container."
 )
 
 [ ! -z "$WEAVE_HOST_CIDR" ] && (
   echo "Binding ip $WEAVE_HOST_CIDR to host..."
-  weave expose $WEAVE_HOST_CIDR || ip addr change $WEAVE_HOST_CIDR dev weave
-  iptables -D FORWARD -i docker0 -o weave -j DROP
+  [ -z "`ip addr show weave | grep $WEAVE_HOST_CIDR`" ] && (
+    weave expose || weave expose
+  )
+  [ -z "`ip addr show weave | grep $WEAVE_HOST_CIDR`" ] && (
+    ip addr change $WEAVE_HOST_CIDR dev weave
+  )
+  iptables -I FORWARD -o weave -j ACCEPT
+  iptables -I FORWARD -i weave -j ACCEPT
   [ ! -z "$WEAVE_ROUTE_GATEWAY" ] && (
     while [ `ip route | grep -e ^default | wc -l` -gt 0 ]; do ( route delete default ) done
     route add default gw $WEAVE_ROUTE_GATEWAY
   )
 )
 
-docker events --since "1970-01-01" -f "event=start" -f "event=create" | while read event; do (
-  CID=`echo $event | awk '{print $4}'`
-  CIDR_ENV=`docker inspect -f "{{range .Config.Env}}{{println .}}{{end}}" $CID | grep -e ^WEAVE_IP=`
+echo "Binding weave ip for all containers with env WEAVE_CIDR..."
+
+(docker ps --no-trunc -q && docker events --since=`date +%s` -f "event=start" -f "event=create" | awk '{print $4}') | while read CID; do (
+  CIDR_ENV=`docker inspect -f "{{range .Config.Env}}{{println .}}{{end}}" $CID | grep -e ^WEAVE_CIDR=`
   [ ! -z "$CIDR_ENV" ] && (
     export $CIDR_ENV
-    echo "Setting ip ${WEAVE_IP} for container id ${CID}"
-    docker network connect --ip=${WEAVE_IP} ${NETWORK_NAME} ${CID}
+    echo "Setting ip ${WEAVE_CIDR} for container id ${CID}"
+    weave attach ${WEAVE_CIDR} ${CID}
   )
 ) done
